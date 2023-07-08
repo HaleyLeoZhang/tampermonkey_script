93085fe349c5b8c6aded3134d1abb3e9e3965f3a
lex <hlzblog@vip.qq.com>
2023-07-08 15:54:57 +0800

    initial

--- yinghuavideo.js
+++ yinghuavideo.js
@@ -4,7 +4,8 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       github.com/HaleyLeoZhang
-// @match        www.yinghuavideo.com/v/*.html
+// @match        www.yinghuavideo.com
+// @match        www.yinghuavideo.com/*
 // @icon         http://www.hlzblog.top/favicon.ico
 // @grant        none
 // ==/UserScript==
@@ -14,6 +15,7 @@
 
     class AdClear {
         // 私有变量
+        debug = false // 判断是否调试  true: 开启日志 false:关闭日志
         second = 1 // 循环检测时间定时器
         constructor() {
             // -
@@ -28,10 +30,14 @@
 
         // - 显示播放地址
         showPlayUrl() {
+            // 仅播放页面才生效
+            if (location.href.match(/v\/.*?.html/) === null) {
+                return
+            }
             let m3u8Url = $("#playbox").attr("data-vid").split("$")[0]
             let html = `
-            <h1 style="font-size:50px">视频播放地址</h1>
-            <h3 style="font-size:30px">${m3u8Url}</h3>
+            <h1 style="font-size:50px;color:blue;font-weight:800;margin-top:50px">视频播放地址</h1>
+            <input  style="font-size:30px" value="${m3u8Url}" placeholder="暂时没有抓到地址">
         `
             $("body").prepend(html)
         }
@@ -46,13 +52,13 @@
                 if (temp === null) {
                     if (indexInter > 0) {
                         clearInterval(indexInter)
-                        console.log("ad removed")
+                        _this.output("adSide removed")
                     }
                 } else {
                     temp.remove()
                     document.querySelector("#HMcoupletDivleft").remove()
                 }
-                console.log("checking ad two side")
+                _this.output("checking ad adSide")
             }, 1000 * _this.second)
         }
 
@@ -66,13 +72,21 @@
                 if (temp === null) {
                     if (indexInter > 0) {
                         clearInterval(indexInter)
-                        console.log("ad removed")
+                        _this.output("adBottom removed")
                     }
                 } else {
                     temp.parentNode.remove()
                 }
-                console.log("checking ad bottom")
+                _this.output("checking ad adBottom")
             }, 1000 * _this.second)
+        }
+
+        // 日志处理
+        output(d) {
+            let _this = this
+            if (this.debug) {
+                _this.output(d)
+            }
         }
     }
 
